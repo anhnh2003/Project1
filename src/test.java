108f17d67a02ce02b8904fd394d5dc25eec3d82e
hoanghn61 <96099627+hoanghn61@users.noreply.github.com>
2023-06-03 16:56:00 +0700

    after fix

--- src/test.java
+++ src/test.java
@@ -1,8 +1,15 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+/* 
 import java.util.ArrayList;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+*/
 public class test {
+/* 
     public static void main(String[] args) throws Exception{
         /*Document doc = Jsoup.connect("https://github.com/redcanaryco/atomic-red-team/tree/master/atomics").get();
         Elements elements = doc.select("a.Link--primary[href*='/atomics/']");
@@ -18,6 +25,7 @@ public class test {
             Elements e = d.select(cssquery);
             res.add(e.text() + "\n");
         }*/
+        /* 
         String u = "https://raw.githubusercontent.com/redcanaryco/atomic-red-team/master/atomics/T1003.001/T1003.001.md";
         // đây là dạng link lưu trữ file github dưới dạng rawtext -> toàn bộ thông tin text đều đc lưu trữ trong thẻ <pre> của file html này
         System.out.println(Jsoup.connect(u).get()); // lấy toàn bộ nội dung html 
@@ -27,9 +35,35 @@ public class test {
     tuy nhiên bằng 1 cách nào đó mà nó k lấy đc thẻ <pre>. Đã tư duy & tra gg+AI and đang khắc phục
     Nếu thành công thì mình đã lấy được nội dung url dưới dạng raw text. Nhưng vẫn lặp đệ quy đến các link con để lấy tiếp content của các link đấy.
      */
-    public static String rawText(String url) throws Exception{
+  /*   public static String rawText(String url) throws Exception{
         Document document = Jsoup.connect(url).get();
         Elements rawText = document.select("pre");
         return rawText.text();
     }
+}*/
+public static void main(String[] args) {
+    String urlStr = "https://raw.githubusercontent.com/redcanaryco/atomic-red-team/master/atomics/T1003.001/T1003.001.md";
+    try {
+        URL url = new URL(urlStr);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setDoOutput(true);
+        InputStream content = connection.getInputStream();
+        String fileContent = inputStreamToString(content);
+        System.out.println(fileContent);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+private static String inputStreamToString(InputStream is) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    String line;
+    BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+    while ((line = br.readLine()) != null) {
+        sb.append(line);
+        sb.append(System.lineSeparator());
+    }
+    return sb.toString();
+}
 }
