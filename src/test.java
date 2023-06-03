6e1660e6144d65773532d31516f6517e4484b1ef
anhnh2003 <anh.nhcoder@gmail.com>
2023-06-03 11:21:21 +0700

    đã tim dc link raw file tren github

--- src/test.java
+++ src/test.java
@@ -1,31 +1,38 @@
-
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
 import java.util.ArrayList;
-
+import java.util.concurrent.ArrayBlockingQueue;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 public class test {
     public static void main(String[] args) throws Exception{
-        Document doc = Jsoup.connect("https://github.com/redcanaryco/atomic-red-team/tree/master/atomics").get();
+        /*Document doc = Jsoup.connect("https://github.com/redcanaryco/atomic-red-team/tree/master/atomics").get();
         Elements elements = doc.select("a.Link--primary[href*='/atomics/']");
         ArrayList<String> text = new ArrayList<String>();
         for (Element element : elements) {
             text.add(element.text());
         }
-        System.out.println(text);
-    String res = "";
-    for(String i : text){
-        String url = "https://github.com/redcanaryco/atomic-red-team/tree/master/atomics/"+ i;
-        Document d = Jsoup.connect(url).get();
-        String cssquery = "a.Link--primary[href*='/atomics/"+i+"/']";
-        Elements e = d.select(cssquery);
-        res += e.text() + "\n";
+        ArrayList<String> res = new ArrayList<String>();
+        for(String i : text){
+            String url = "https://github.com/redcanaryco/atomic-red-team/tree/master/atomics/"+ i;
+            Document d = Jsoup.connect(url).get();
+            String cssquery = "a.Link--primary[href*='/atomics/"+i+"/']";
+            Elements e = d.select(cssquery);
+            res.add(e.text() + "\n");
+        }*/
+        String u = "https://raw.githubusercontent.com/redcanaryco/atomic-red-team/master/atomics/T1003.001/T1003.001.md";
+        // đây là dạng link lưu trữ file github dưới dạng rawtext -> toàn bộ thông tin text đều đc lưu trữ trong thẻ <pre> của file html này
+        System.out.println(Jsoup.connect(u).get()); // lấy toàn bộ nội dung html 
     }
-    System.out.println(res);
-    System.out.print("...");
-}
+    /**
+    đang tính tạo hàm này để lấy thông tin từ link raw ở trên
+    tuy nhiên bằng 1 cách nào đó mà nó k lấy đc thẻ <pre>. Đã tư duy & tra gg+AI and đang khắc phục
+    Nếu thành công thì mình đã lấy được nội dung url dưới dạng raw text. Nhưng vẫn lặp đệ quy đến các link con để lấy tiếp content của các link đấy.
+     */
+    public static String rawText(String url) throws Exception{
+        Document document = Jsoup.connect(url).get();
+        Element rawText = document.select("pre");
+        return rawText.text();
+    }
 }
