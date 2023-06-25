e3a07c2872457d6ebf341b373bfd6c3a39935b61
hoanghn61 <96099627+hoanghn61@users.noreply.github.com>
2023-06-25 17:38:28 +0700

    update 25/06

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 package com.simplilearn.mavenproject;
 
 import java.util.List;
+import java.util.ArrayList;
 
 
 public class App {
@@ -18,12 +19,30 @@ public class App {
         }
         System.out.print("Number of techniques and subtechniques in AtomicRedTeam: " + count);
         */
-        
+        List<List<Object>> data = new ArrayList<>();
+
+        // Add header row
+        data.add(List.of("Technique ID", "Technique Name", "Test Case", "Supported Platforms"));
+
         List<String> techniqueIds = techniques.crawlTechniqueIds();
         for (String techniqueId : techniqueIds) {
             AtomicRedTeamTechnique technique = dataExtractor.fetchTechnique(techniqueId);
-            System.out.println(technique.formatAsText());
+          //  System.out.println(technique.formatAsText());
+            String techniqueName = technique.getTechniqueName();
+
+            for (AtomicRedTeamTestCase atomicTest : technique.getAtomicTests()) {
+                List<Object> row = new ArrayList<>();
+                row.add(techniqueId);
+                row.add(techniqueName);
+                row.add(atomicTest.getName());
+                row.add(String.join(", ", atomicTest.getSupportedPlatforms()));
+                data.add(row);
         }
     }
+        
+        AtomicExcelExporter.exportToExcel(data, "C:\\Users\\Admin\\Documents\\atomic_red_team_data.xlsx");
+
+  }
 }
+   
 
