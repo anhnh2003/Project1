99c820a2b9f98916fda96bc23875d8a05d8d8933
hoanghn61 <96099627+hoanghn61@users.noreply.github.com>
2023-07-01 19:58:13 +0700

    fix with jenkins

--- src/AtomicRedTeamTechniqueId.java
+++ src/AtomicRedTeamTechniqueId.java
@@ -20,7 +20,6 @@ public class AtomicRedTeamTechniqueId {
     private static final String IGNORED_FILES_PATTERN_START = "^00-";
     private static final String IGNORED_FILES_PATTERN_END = "^mordor_";
 
-
     public List<String> crawlTechniqueIds() throws IOException {
         List<String> techniqueIds = new ArrayList<>();
 
@@ -35,39 +34,39 @@ public class AtomicRedTeamTechniqueId {
         Pattern subtechIdPattern = Pattern.compile(SUBTECHNIQUE_ID_PATTERN);
         Pattern ignoredFilesPatternStart = Pattern.compile(IGNORED_FILES_PATTERN_START);
         Pattern ignoredFilesPatternEnd = Pattern.compile(IGNORED_FILES_PATTERN_END);
-     
-        boolean shouldContinue = false;
+
         for (Element link : links) {
             String href = link.attr("href");
 
-            // Filter out the first and last files
-            if (href.equals("/redcanaryco/atomic-red-team/tree/master/atomics") || href.equals("/redcanaryco/atomic-red-team/tree/master/atomics/")) {
-                shouldContinue = true;
+            if (isFirstOrLastFile(href)) {
+                continue;
             }
 
-            // Filter out files that match the ignored files patterns
             String fileName = Paths.get(href).getFileName().toString();
-            if (!shouldContinue && (ignoredFilesPatternStart.matcher(fileName).find() || ignoredFilesPatternEnd.matcher(fileName).find())) {
-                shouldContinue = true;
+            if (isFileIgnored(fileName, ignoredFilesPatternStart, ignoredFilesPatternEnd)) {
+                continue;
             }
 
-            if (!shouldContinue) {
-                // Extract the technique and sub-technique IDs from the file name
-                String techniqueId = extractTechniqueId(fileName, techIdPattern);
-                String subtechniqueId = extractTechniqueId(fileName, subtechIdPattern);
-                if (techniqueId != null) {
-                    techniqueIds.add(techniqueId);
-                }
-                if (subtechniqueId != null) {
-                    techniqueIds.add(subtechniqueId);
-                }
+            String techniqueId = extractTechniqueId(fileName, techIdPattern);
+            String subtechniqueId = extractTechniqueId(fileName, subtechIdPattern);
+            if (techniqueId != null) {
+                techniqueIds.add(techniqueId);
             }
-
-            shouldContinue = false;
+            if (subtechniqueId != null) {
+                techniqueIds.add(subtechniqueId);
+            }
         }
 
-
         return techniqueIds;
+    }
+
+    private boolean isFirstOrLastFile(String href) {
+        return href.equals("/redcanaryco/atomic-red-team/tree/master/atomics")
+                || href.equals("/redcanaryco/atomic-red-team/tree/master/atomics/");
+    }
+
+    private boolean isFileIgnored(String fileName, Pattern ignoredFilesPatternStart, Pattern ignoredFilesPatternEnd) {
+        return ignoredFilesPatternStart.matcher(fileName).find() || ignoredFilesPatternEnd.matcher(fileName).find();
     }
 
     public String extractTechniqueId(String fileName, Pattern idPattern) {
@@ -76,9 +75,6 @@ public class AtomicRedTeamTechniqueId {
             return matcher.group();
         } else {
             return null;
-        }
-        
-        
+        }  
     }
-    
 }
