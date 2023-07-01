43e39e6a74428eaa0fcbe8fc599614991f2af727
hoanghn61 <96099627+hoanghn61@users.noreply.github.com>
2023-07-01 19:39:47 +0700

    fix with jenkins

--- src/AtomicRedTeamTechniqueId.java
+++ src/AtomicRedTeamTechniqueId.java
@@ -17,8 +17,8 @@ public class AtomicRedTeamTechniqueId {
 	private static final String ATOMICS_URL = "https://github.com/redcanaryco/atomic-red-team/tree/master/atomics";
 	private static final String TECHNIQUE_ID_PATTERN = "^T\\d{4}$";
 	private static final String SUBTECHNIQUE_ID_PATTERN = "^T\\d{4}\\.\\d{3}$";
-    private static final  String IGNORED_FILES_PATTERN_START = "^00-";
-    private static final  String IGNORED_FILES_PATTERN_END = "^mordor_";
+    private static final String IGNORED_FILES_PATTERN_START = "^00-";
+    private static final String IGNORED_FILES_PATTERN_END = "^mordor_";
 
 
     public List<String> crawlTechniqueIds() throws IOException {
@@ -35,30 +35,37 @@ public class AtomicRedTeamTechniqueId {
         Pattern subtechIdPattern = Pattern.compile(SUBTECHNIQUE_ID_PATTERN);
         Pattern ignoredFilesPatternStart = Pattern.compile(IGNORED_FILES_PATTERN_START);
         Pattern ignoredFilesPatternEnd = Pattern.compile(IGNORED_FILES_PATTERN_END);
+     
+        boolean shouldContinue = false;
         for (Element link : links) {
             String href = link.attr("href");
 
             // Filter out the first and last files
             if (href.equals("/redcanaryco/atomic-red-team/tree/master/atomics") || href.equals("/redcanaryco/atomic-red-team/tree/master/atomics/")) {
-                continue;
+                shouldContinue = true;
             }
 
             // Filter out files that match the ignored files patterns
             String fileName = Paths.get(href).getFileName().toString();
-            if (ignoredFilesPatternStart.matcher(fileName).find() || ignoredFilesPatternEnd.matcher(fileName).find()) {
-                continue;
+            if (!shouldContinue && (ignoredFilesPatternStart.matcher(fileName).find() || ignoredFilesPatternEnd.matcher(fileName).find())) {
+                shouldContinue = true;
             }
 
-            // Extract the technique and sub-technique IDs from the file name
-            String techniqueId = extractTechniqueId(fileName, techIdPattern);
-            String subtechniqueId = extractTechniqueId(fileName, subtechIdPattern);
-            if (techniqueId != null) {
-                techniqueIds.add(techniqueId);
+            if (!shouldContinue) {
+                // Extract the technique and sub-technique IDs from the file name
+                String techniqueId = extractTechniqueId(fileName, techIdPattern);
+                String subtechniqueId = extractTechniqueId(fileName, subtechIdPattern);
+                if (techniqueId != null) {
+                    techniqueIds.add(techniqueId);
+                }
+                if (subtechniqueId != null) {
+                    techniqueIds.add(subtechniqueId);
+                }
             }
-            if (subtechniqueId != null) {
-                techniqueIds.add(subtechniqueId);
-            }
+
+            shouldContinue = false;
         }
+
 
         return techniqueIds;
     }
